c8bea0e6e8885ff92678c5b3f1b028bb9342c18d
seliv <aseliv@gmail.com>
2020-12-04 04:28:28 +0300

    Drag and zoom implemented.

--- src/main/kotlin/GomokuMain.kt
+++ src/main/kotlin/GomokuMain.kt
@@ -7,8 +7,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Canvas
+import androidx.compose.material.Surface
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import example.imageviewer.view.DragHandler
+import example.imageviewer.view.Draggable
+import example.imageviewer.view.ScaleHandler
+import example.imageviewer.view.Zoomable
 
 fun main() = Window {
     var text by remember { mutableStateOf("Hello, World!") }
@@ -30,11 +35,31 @@ fun main() = Window {
 private fun Board(
     boardModel: BoardModel
 ) {
-    drawBoardInternal(boardModel)
+    val dragHandler = DragHandler()
+    val scaleHandler = ScaleHandler()
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Draggable(
+            onDrag = dragHandler,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Zoomable(
+                onScale = scaleHandler,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                drawBoardInternal(boardModel, scaleHandler, dragHandler)
+            }
+        }
+    }
 }
 
 @Composable
-private fun drawBoardInternal(boardModel: BoardModel) {
+private fun drawBoardInternal(
+    boardModel: BoardModel,
+    scaleHandler: ScaleHandler,
+    dragHandler: DragHandler
+) {
     Canvas(modifier = Modifier.fillMaxSize()) {
         var minX = boardModel.minX()
         var minY = boardModel.minY()
@@ -59,7 +84,8 @@ private fun drawBoardInternal(boardModel: BoardModel) {
                 for (yd in boardModel.minY()..boardModel.maxY()) {
                     val x = (xd - minX) * cellWidth
                     val y = (yd - minY) * cellHeight
-                    drawCircle(Color.Black, 2.0f, Offset(x, y))
+                    drawCircle(Color.Black, 2.0f,
+                        dragHandler.translate(scaleHandler.translate(Offset(x, y))))
                 }
             }
         }
@@ -67,7 +93,8 @@ private fun drawBoardInternal(boardModel: BoardModel) {
         for (piece in boardModel.pieces) {
             val x = (piece.key.x - minX) * cellWidth
             val y = (piece.key.y - minY) * cellHeight
-            drawCircle(piece.value.color, 10.0f, Offset(x, y))
+            drawCircle(piece.value.color, 10.0f,
+                dragHandler.translate(scaleHandler.translate(Offset(x, y))))
         }
     }
 }
---
+++ src/main/kotlin/example/imageviewer/view/Scalable.kt
@@ -0,0 +1,54 @@
+package example.imageviewer.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.gesture.RawScaleObserver
+import androidx.compose.ui.gesture.doubleTapGestureFilter
+import androidx.compose.ui.gesture.rawScaleGestureFilter
+import androidx.compose.ui.Modifier
+import androidx.compose.material.Surface
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import kotlin.math.pow
+
+@Composable
+fun Scalable (
+    onScale: ScaleHandler,
+    modifier: Modifier = Modifier,
+    children: @Composable() () -> Unit
+) {
+    Surface(
+        color = Color.Transparent,
+        modifier = modifier.rawScaleGestureFilter(
+            scaleObserver = onScale,
+            canStartScaling = { true }
+        ).doubleTapGestureFilter(onDoubleTap = { onScale.resetFactor() }),
+    ) {
+        children()
+    }
+}
+
+class ScaleHandler(private val maxFactor: Float = 5f, private val minFactor: Float = -10f) :
+    RawScaleObserver {
+    val factor = mutableStateOf(1f)
+
+    fun resetFactor() {
+        if (factor.value > minFactor)
+            factor.value = minFactor
+    }
+
+    override fun onScale(scaleFactor: Float): Float {
+        factor.value += scaleFactor - 1f
+
+        if (maxFactor < factor.value) factor.value = maxFactor
+        if (minFactor > factor.value) factor.value = minFactor
+
+        return scaleFactor
+    }
+
+    fun translate(offset: Offset): Offset {
+        val scale = factor.value.toDouble().pow(1.4).toFloat()
+        return Offset(offset.x * scale, offset.y * scale)
+    }
+}
